a02921c7471e8914cc438938e8da684baa8ca901
colto <colto@DESKTOP-2M67LJ5>
2023-05-25 23:16:14 +0300

    plm

---
+++ NicoRealSolution/Migrations/20230525200408_174yr.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NicoRealSolution.Migrations
+{
+    public partial class _174yr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DatePosted",
+                table: "Properties",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DatePosted",
+                table: "Properties",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
