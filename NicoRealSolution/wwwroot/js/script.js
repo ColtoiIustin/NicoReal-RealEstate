5a12fbe0a1830cde82e11e84d0f80e8f5ebd0a25
colto <colto@DESKTOP-2M67LJ5>
2023-07-18 13:52:44 +0300

    74jkt68tb

--- NicoRealSolution/wwwroot/js/script.js
+++ NicoRealSolution/wwwroot/js/script.js
@@ -365,4 +365,13 @@
 
 })(window.jQuery);
 
+function googleTranslateElementInit() {
+   
+    new google.translate.TranslateElement({
+        pageLanguage: 'ro', 
+        includedLanguages: 'en,de', 
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+    }, 'google_translate_element');
+}
+
 
